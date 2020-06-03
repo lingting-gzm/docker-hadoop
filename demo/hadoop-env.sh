590f2ecb426224fd3aaba3ee8d2d6f68e34b1056
lingting-gzm <59352063+lingting-gzm@users.noreply.github.com>
2020-06-03 20:31:26 +0800

    Add files via upload

---
+++ demo/hadoop-env.sh
@@ -0,0 +1,7 @@
+export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
+export HDFS_NAMENODE_USER=root
+export HDFS_DATANODE_USER=root
+export HDFS_SECONDARYNAMENODE_USER=root
+export YARN_RESOURCEMANAGER_USER=root
+export YARN_NODEMANAGER_USER=root
+export HADOOP_HOME=/opt/hadoop
